2b62afc0c21437207f96ab4f49e32f16a0c68cd2
Ed McManus <edmcmanus@champ.local>
2010-03-02 03:45:24 -0800

    Basic styling

---
+++ public/javascripts/swfupload/handlers.js
@@ -0,0 +1,215 @@
+/* Demo Note:  This demo uses a FileProgress class that handles the UI for displaying the file name and percent complete.
+The FileProgress class is not part of SWFUpload.
+*/
+
+
+/* **********************
+   Event Handlers
+   These are my custom event handlers to make my
+   web application behave the way I went when SWFUpload
+   completes different tasks.  These aren't part of the SWFUpload
+   package.  They are part of my application.  Without these none
+   of the actions SWFUpload makes will show up in my application.
+   ********************** */
+function fileQueued(file) {
+	try {
+		var progress = new FileProgress(file, window.swfu.customSettings.progressTarget);
+		progress.setStatus("Pending...");
+		progress.toggleCancel(true, this);
+
+	} catch (ex) {
+		window.swfu.debug(ex);
+	}
+
+}
+
+function fileQueueError(file, errorCode, message) {
+	try {
+		if (errorCode === SWFUpload.QUEUE_ERROR.QUEUE_LIMIT_EXCEEDED) {
+			alert("You have attempted to queue too many files.\n" + (message === 0 ? "You have reached the upload limit." : "You may select " + (message > 1 ? "up to " + message + " files." : "one file.")));
+			return;
+		}
+
+		var progress = new FileProgress(file, window.swfu.customSettings.progressTarget);
+		progress.setError();
+		progress.toggleCancel(false);
+
+		switch (errorCode) {
+		case SWFUpload.QUEUE_ERROR.FILE_EXCEEDS_SIZE_LIMIT:
+			progress.setStatus("File is too big.");
+			window.swfu.debug("Error Code: File too big, File name: " + file.name + ", File size: " + file.size + ", Message: " + message);
+			break;
+		case SWFUpload.QUEUE_ERROR.ZERO_BYTE_FILE:
+			progress.setStatus("Cannot upload Zero Byte files.");
+			window.swfu.debug("Error Code: Zero byte file, File name: " + file.name + ", File size: " + file.size + ", Message: " + message);
+			break;
+		case SWFUpload.QUEUE_ERROR.INVALID_FILETYPE:
+			progress.setStatus("Invalid File Type.");
+			window.swfu.debug("Error Code: Invalid File Type, File name: " + file.name + ", File size: " + file.size + ", Message: " + message);
+			break;
+		default:
+			if (file !== null) {
+				progress.setStatus("Unhandled Error");
+			}
+			window.swfu.debug("Error Code: " + errorCode + ", File name: " + file.name + ", File size: " + file.size + ", Message: " + message);
+			break;
+		}
+	} catch (ex) {
+        window.swfu.debug(ex);
+    }
+}
+
+function fileDialogComplete(numFilesSelected, numFilesQueued) {
+	try {
+		if (numFilesSelected > 0) {
+			document.getElementById(window.swfu.customSettings.cancelButtonId).disabled = false;
+		}
+		
+		/* I want auto start the upload and I can do that here */
+		window.swfu.startUpload();
+	} catch (ex)  {
+        window.swfu.debug(ex);
+	}
+}
+
+function uploadStart(file)
+{	
+	var policyRequestSuccess = false;
+	
+	try
+	{
+		// Get a new, validated, upload policy for this file
+		new Ajax.Request('/upload/get_upload_key?' + Object.toQueryString(file), {
+			method:'get',
+			asynchronous: false,
+			onSuccess: function( transport ){
+				policyRequestSuccess = true;
+				window.swfu.debug("TRANSPORT FOLLOWING:");
+				window.swfu.debug(transport.responseJSON);
+				window.swfu.setPostParams( transport.responseJSON );
+			},
+			onException: function( transport, e ){
+				window.debug(e);
+				policyRequestSuccess = false;
+			},
+			onFailure: function( file, e ){
+				window.debug(e);
+				policyRequestSuccess = false;
+			}
+		});
+		
+		// Fire initial progress event
+		var progress = new FileProgress(file, window.swfu.customSettings.progressTarget);
+		progress.setStatus("Uploading...");
+		progress.toggleCancel(true, this);
+	}
+	catch (ex)
+	{
+		policyRequestSuccess = false;
+	}
+	
+	if (!policyRequestSuccess)
+	{
+		// Display error
+		uploadError( file, SWFUpload.UPLOAD_ERROR.FILE_VALIDATION_FAILED, "S3 Upload policy not granted." );
+	}
+	
+	return policyRequestSuccess;
+}
+
+function uploadProgress(file, bytesLoaded, bytesTotal) {
+	try {
+		var percent = Math.ceil((bytesLoaded / bytesTotal) * 100);
+
+		var progress = new FileProgress(file, window.swfu.customSettings.progressTarget);
+		progress.setProgress(percent);
+		progress.setStatus("Uploading...");
+	} catch (ex) {
+		window.swfu.debug(ex);
+	}
+}
+
+function uploadSuccess(file, serverData) {
+	new Ajax.Request('/upload/game_upload_done?' + Object.toQueryString(file), {
+		method:'get',
+		asynchronous: false,
+		onSuccess: function(){
+			var progress = new FileProgress(file, window.swfu.customSettings.progressTarget);
+			progress.setStatus("Sending meta data.");
+		}
+	});
+	
+	try {
+		var progress = new FileProgress(file, window.swfu.customSettings.progressTarget);
+		progress.setComplete();
+		progress.setStatus("Complete.");
+		progress.toggleCancel(false);
+
+	} catch (ex) {
+		window.swfu.debug(ex);
+	}
+}
+
+function uploadError(file, errorCode, message) {
+	try {
+		var progress = new FileProgress(file, window.swfu.customSettings.progressTarget);
+		progress.setError();
+		progress.toggleCancel(false);
+
+		switch (errorCode) {
+		case SWFUpload.UPLOAD_ERROR.HTTP_ERROR:
+			progress.setStatus("Upload Error: " + message);
+			window.swfu.debug("Error Code: HTTP Error, File name: " + file.name + ", Message: " + message);
+			break;
+		case SWFUpload.UPLOAD_ERROR.UPLOAD_FAILED:
+			progress.setStatus("Upload Failed.");
+			window.swfu.debug("Error Code: Upload Failed, File name: " + file.name + ", File size: " + file.size + ", Message: " + message);
+			break;
+		case SWFUpload.UPLOAD_ERROR.IO_ERROR:
+			progress.setStatus("Server (IO) Error");
+			window.swfu.debug("Error Code: IO Error, File name: " + file.name + ", Message: " + message);
+			break;
+		case SWFUpload.UPLOAD_ERROR.SECURITY_ERROR:
+			progress.setStatus("Security Error");
+			window.swfu.debug("Error Code: Security Error, File name: " + file.name + ", Message: " + message);
+			break;
+		case SWFUpload.UPLOAD_ERROR.UPLOAD_LIMIT_EXCEEDED:
+			progress.setStatus("Upload limit exceeded.");
+			window.swfu.debug("Error Code: Upload Limit Exceeded, File name: " + file.name + ", File size: " + file.size + ", Message: " + message);
+			break;
+		case SWFUpload.UPLOAD_ERROR.FILE_VALIDATION_FAILED:
+			progress.setStatus("Failed Validation.  Upload skipped.");
+			window.swfu.debug("Error Code: File Validation Failed, File name: " + file.name + ", File size: " + file.size + ", Message: " + message);
+			break;
+		case SWFUpload.UPLOAD_ERROR.FILE_CANCELLED:
+			// If there aren't any files left (they were all cancelled) disable the cancel button
+			if (window.swfu.getStats().files_queued === 0) {
+				document.getElementById(window.swfu.customSettings.cancelButtonId).disabled = true;
+			}
+			progress.setStatus("Cancelled");
+			progress.setCancelled();
+			break;
+		case SWFUpload.UPLOAD_ERROR.UPLOAD_STOPPED:
+			progress.setStatus("Stopped");
+			break;
+		default:
+			progress.setStatus("Unhandled Error: " + errorCode);
+			window.swfu.debug("Error Code: " + errorCode + ", File name: " + file.name + ", File size: " + file.size + ", Message: " + message);
+			break;
+		}
+	} catch (ex) {
+        window.swfu.debug(ex);
+    }
+}
+
+function uploadComplete(file) {
+	if (window.swfu.getStats().files_queued === 0) {
+		document.getElementById(window.swfu.customSettings.cancelButtonId).disabled = true;
+	}
+}
+
+// This event comes from the Queue Plugin
+function queueComplete(numFilesUploaded) {
+	var status = document.getElementById("divStatus");
+	status.innerHTML = numFilesUploaded + " file" + (numFilesUploaded === 1 ? "" : "s") + " uploaded.";
+}
