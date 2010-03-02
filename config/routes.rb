ee374db9bbfc29c08daefbc3776fb285930998f3
Ed McManus <edmcmanus@champ.local>
2010-03-02 03:07:15 -0800

    Added user system - authlogic

--- config/routes.rb
+++ config/routes.rb
@@ -32,7 +32,13 @@ ActionController::Routing::Routes.draw do |map|
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
   # map.root :controller => "welcome"
-
+  
+  map.resource :user_session
+  map.root :controller => "user_sessions", :action => "new" # optional, this just sets the root route
+  
+  map.resource :account, :controller => "users"
+  map.resources :users
+  
   # See how all your routes lay out with "rake routes"
 
   # Install the default routes as the lowest priority.
